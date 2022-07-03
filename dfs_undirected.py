7801a370092c5160c672781f3f6b56211d37773d
Harsh Raj <harsh.smartters@gmail.com>
2022-07-04 03:40:57 +0530

    updated dfs undirected

--- dfs_undirected.py
+++ dfs_undirected.py
@@ -1,5 +1,6 @@
-from tkinter.font import families
+from distutils.command.build import build
 from data import undirected_graph_data
+from build_graph import build_graph
 
 def dfs_using_recursion(graph, node, visited=set()):
     visited.add(node)
@@ -10,15 +11,9 @@ def dfs_using_recursion(graph, node, visited=set()):
             count += dfs_using_recursion(graph, child, visited)  # passing visited here has zero effect since visited will automatically have memory of visited nodes reference in recursive call
     count += 1
 
-graph = {}
 nodes = undirected_graph_data['nodes']
 edges = undirected_graph_data['edges']
-for node in nodes:
-    graph[node] = set()
-
-for (u, v) in edges:
-    graph[u].add(v)
-    graph[v].add(u) 
+graph = build_graph(nodes=nodes, edges=edges)
 
 visited = set()
 families = []
